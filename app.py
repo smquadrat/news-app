633bb6ac9c774a0f793521153aac00089ee7c7d8
Sebastian Quadrat <smquadrat@gmail.com>
2023-04-03 11:24:45 -0400

    Update

--- app.py
+++ app.py
@@ -11,23 +11,26 @@ def all_top_headlines():
     ft_url = "https://www.ft.com/?format=rss"
     economist_url = "https://www.economist.com/latest/rss.xml"
     fp_url = "https://foreignpolicy.com/feed/"
-    tc_url = "https://techcrunch.com/feed/"
     hn_url = "https://news.ycombinator.com/rss"
+    politico_url = "https://rss.politico.com/politics-news.xml"
+    techcrunch_url = "https://techcrunch.com/feed/"
     bbc_feed = feedparser.parse(bbc_url)
     nyt_feed = feedparser.parse(nyt_url)
     ft_feed = feedparser.parse(ft_url)
     economist_feed = feedparser.parse(economist_url)
     fp_feed = feedparser.parse(fp_url)
-    tc_feed = feedparser.parse(tc_url)
     hn_feed = feedparser.parse(hn_url)
+    politico_feed = feedparser.parse(politico_url)
+    techcrunch_feed = feedparser.parse(techcrunch_url)
     bbc_headlines = bbc_feed.entries[:5]
     nyt_headlines = nyt_feed.entries[:5]
     ft_headlines = ft_feed.entries[:5]
     economist_headlines = economist_feed.entries[:5]
     fp_headlines = fp_feed.entries[:5]
-    tc_headlines = tc_feed.entries[:5]
     hn_headlines = hn_feed.entries[:5]
-    return render_template('index.html', title='All Top Headlines', bbc_headlines=bbc_headlines, nyt_headlines=nyt_headlines, ft_headlines=ft_headlines, economist_headlines=economist_headlines, fp_headlines=fp_headlines, tc_headlines=tc_headlines, hn_headlines=hn_headlines)
+    politico_headlines = politico_feed.entries[:5]
+    techcrunch_headlines = techcrunch_feed.entries[:5]
+    return render_template('index.html', title='All Top Headlines', bbc_headlines=bbc_headlines, nyt_headlines=nyt_headlines, ft_headlines=ft_headlines, economist_headlines=economist_headlines, fp_headlines=fp_headlines, hn_headlines=hn_headlines, politico_headlines=politico_headlines, techcrunch_headlines=techcrunch_headlines)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
